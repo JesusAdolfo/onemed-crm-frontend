59387c8aecafc8ad15a9a3a6ca724bdd6020f458
Jesus Adolfo <rodriguezjesus@gmail.com@>
2016-09-15 13:09:47 +0900

    fix

--- master/js/app.module.js
+++ master/js/app.module.js
@@ -11,8 +11,8 @@
 
 // APP START
 // ----------------------------------- 
-var globalUri = "http://localhost:9000/";
-// var globalUri = "https://portal.onemed.us:9001/";
+// var globalUri = "http://localhost:9000/";
+var globalUri = "https://portal.onemed.us:9001/";
 (function() {
     'use strict';
 
